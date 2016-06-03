5a9db147f5dac4cb2410b64a1e634ad55ae0ef92
aball6412 <aaron.thompkins12@gmail.com>
2016-06-03 12:41:17 -0400

    set up ejs as template engine and linked a stylesheet

--- server.js
+++ server.js
@@ -2,15 +2,22 @@ var express = require("express");
 var app = express();
 
 
+//Set up port for production and development
 var port = process.env.PORT || 3000;
 
+
+//Serve static files
 app.use("/", express.static(__dirname + "/public"));
 
+//Set up EJS as view engine
+app.set("view engine", "ejs");
 
 
+
+//Respond to homepage URL get requests.
 app.get("/", function(request, response) {
     
-    response.send("Hello World");
+    response.render("index");
     
 });
 
